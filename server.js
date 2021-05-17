413f70b52e094e14d94c0722862ee3ea382e8788
ningpop <craft1933@naver.com>
2021-05-18 02:37:44 +0900

    section 1 - express.json()

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 const PORT = 5000;
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
+
+app.use(express.json()); // Frontend로부터 받은 요청에 들어있는 body를 사용하기 위해 express 내장 미들웨어 함수를 사용
+app.get('/', (req, res) => { // Frontend로부터 root route에 get 요청을 받으면
+    res.send('Hello World'); // 응답으로 'Hello World'를 전송
 });
 
 app.listen(PORT);
